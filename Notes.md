236e27f56fd7e36e251f30586248299e1c7f12e0
ZeccaLehn <zgit@outlook.com>
2019-05-02 09:59:32 -0700

    189 React Components

--- Notes.md
+++ Notes.md
@@ -7,3 +7,26 @@ npm start
 
 ### NPM Commands
 https://docs.npmjs.com/cli-documentation/cli
+
+### Note: In Sublime Text, change view with Babel JS for React Syntax
+
+### Updates on React App
+In `package.json`, simply update `"react-scripts"` to a newer version, and rerun `npm start` from Command Line. Note: updating can break features -- so QA is needed post change.
+
+Imports work on the app now, because bundling has been integrated with `create-react-app`
+
+React (view library manipulates DOM)
+
+ReactDOM is used for DOM websites
+
+Can use ReactNative as import for mobile
+
+Can use `import ./index.css` in `index.js` to render individual components from virtual DOM.
+
+Use `registerServiceWorker();` in index.js to work with apps offline -- automatically installed on `npm start`.
+
+### Tachyons allow us to style components
+Exit console
+Enter `npm install tachyons`
+Gets added to package.json
+*Can install without Typescript -- ignore errors
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+// import App from './App';
+import Hello from './Hello.js';
+
+// Add tachyons
+import 'tachyons';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<h2> Removed App tag and commented out import </h2>, document.getElementById('root'));
+// "Props" here within Hello Component are unique to React
+// Props use curly brackets
+// Prop "greetings" can be anything
+ReactDOM.render(<Hello greeting={'Hello' + 'Ninja using JavaScript'} />, document.getElementById('root'));
+ReactDOM.render(<Hello anything={'Antying else applies here...'} />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
