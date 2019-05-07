2957ac0d8d163e2b784faaa91fafd4978165a450
ZeccaLehn <zgit@outlook.com>
2019-05-07 12:00:23 -0700

    Updates

---
+++ precache-manifest.1eae2838e95f26fe5588d5b4f06f788b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a37ccdb6f1d86bf5d1f9e18f9d96631",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/robotfriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "5824da89e6aa9c8d3546",
+    "url": "/robotfriends/static/css/main.964ea729.chunk.css"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/robotfriends/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "5824da89e6aa9c8d3546",
+    "url": "/robotfriends/static/js/main.d25dc22d.chunk.js"
+  },
+  {
+    "revision": "aebb4e0e6c2039467182",
+    "url": "/robotfriends/static/js/runtime~main.6fe61ee8.js"
+  },
+  {
+    "revision": "86c545ccae4a76a95a9ffdd312221e69",
+    "url": "/robotfriends/static/media/MR ROBOT.86c545cc.woff"
+  }
+]);
